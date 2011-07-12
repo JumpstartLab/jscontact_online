cadb38f6e3560b6a93a6d862ff62b6db02492b28
Jeff Casimir <jeff@casimircreative.com>
2011-07-12 10:51:56 -0400

    First tests and validations for Person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,7 +1,22 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe Person do
+  before(:each) do
+    @person = Person.new(:first_name => "Daffy",
+                        :last_name => "Duck")
+  end
+  
   it "should be valid" do
-    Person.new.should be_valid
+    @person.should be_valid
+  end
+  
+  it "should not be valid without a first name" do
+    @person.first_name = nil
+    @person.should_not be_valid 
+  end
+  
+  it "should not be valid without a last name" do
+    @person.last_name = nil
+    @person.should_not be_valid
   end
 end
