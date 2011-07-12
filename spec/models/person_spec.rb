c0339e64487c5d151455b0e48753367cda145328
Jeff Casimir <jeff@casimircreative.com>
2011-07-12 10:57:04 -0400

    Update tests to use let

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe Person do
-  before(:each) do
-    @person = Person.new(:first_name => "Daffy",
-                        :last_name => "Duck")
+  
+  let(:person) do
+    Person.new(:first_name => "Daffy",
+               :last_name => "Duck")
   end
   
   it "should be valid" do
-    @person.should be_valid
+    person.should be_valid
   end
   
   it "should not be valid without a first name" do
-    @person.first_name = nil
-    @person.should_not be_valid 
+    person.first_name = nil
+    person.should_not be_valid 
   end
   
   it "should not be valid without a last name" do
-    @person.last_name = nil
-    @person.should_not be_valid
+    person.last_name = nil
+    person.should_not be_valid
   end
 end
