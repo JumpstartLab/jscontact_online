5d357fb8a4118c6224f53c00497027e6d6d958d4
Jeff Casimir <jeff@casimircreative.com>
2011-07-13 09:45:28 -0400

    Implementing more capybara specs

--- spec/integration/people_views_spec.rb
+++ spec/integration/people_views_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
-require 'capybara/rspec'
 
 describe "the views for people", :type => :request do
   
   context "when looking at a person" do
-    before(:all) do
+    before(:each) do
       @person = Person.create(:first_name => "Daffy", :last_name => "Duck")
       @person.phone_numbers.create(:number => "2223334444")
       @person.phone_numbers.create(:number => "5556667777")
@@ -17,6 +16,10 @@ describe "the views for people", :type => :request do
       end
     end
     
-    it "should have edit links for each phone number"
+    it "should have edit links for each phone number" do
+      @person.phone_numbers.each do |phone_number|    
+        page.should have_link("Edit", :href => edit_phone_number_path(phone_number))
+      end
+    end
   end
 end
