70b03daaafd8775d83523e5963b8bc4cb4f67dd3
Jeff Casimir <jeff@casimircreative.com>
2011-07-13 12:58:00 -0400

    Ran scaffolding for Companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Jscontact::Application.routes.draw do
+  resources :companies
+
   resources :phone_numbers
 
   resources :people
---
+++ spec/fabricators/company_fabricator.rb
@@ -0,0 +1,3 @@
+Fabricator(:company) do
+  name Faker::Company.name
+end
