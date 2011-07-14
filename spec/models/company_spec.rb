f18804dc917eb18da181392bb1abb6a178ec66e9
Jeff Casimir <jeff@casimircreative.com>
2011-07-14 09:39:49 -0400

    Simplifying generated tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company){ Fabricate(:company) }
+
+  it "should be valid" do
+    company.should be_valid
+  end
+  
+  it "should not be valid without a name" do
+    company.name = nil
+    company.should_not be_valid
+  end
 end
