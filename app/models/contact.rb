bfc701613a9531a78d5e1b3938f1d883d2bdee3e
Jeff Casimir <jeff@casimircreative.com>
2011-07-15 09:38:58 -0400

    File uploads almost working

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,9 @@ module Contact
   included do  
     has_many :phone_numbers, :as => :contact
     # has_many :email_addresses, :as => :contact  
-    has_one :mugshot, :as => :contact    
+    has_one :mugshot, :as => :contact
+    accepts_nested_attributes_for :mugshot   
+    attr_accessible :mugshot     
   end
   
   module ClassMethods
@@ -12,6 +14,8 @@ module Contact
   end
   
   module InstanceMethods
-
+    def find_or_build_mugshot
+      self.mugshot || build_mugshot
+    end
   end
 end
--- app/models/mugshot.rb
+++ app/models/mugshot.rb
@@ -1,3 +1,5 @@
 class Mugshot < ActiveRecord::Base
   belongs_to :contact, :polymorphic => true
+  
+  has_attached_file :asset
 end
